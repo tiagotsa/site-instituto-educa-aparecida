a860e6bbb4b4a3e0ecb86a5dc2bcbbcb3547a2d0
Tiago Soares de Almeida <tiago.almeidat@outlook.com>
2026-02-13 13:50:02 -0300

    Supabase conf

--- supabase_config.example.js
+++ supabase_config.example.js
@@ -9,5 +9,4 @@
 window.SUPABASE_CONFIG = {
   SUPABASE_URL: 'https://zatcmwryaqbzfzpbuvml.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_9e45ie6b0_M6tLJ133MTHw_swQfGRS-',
-  EMAIL_ENDPOINT: 'https://formspree.io/f/YOUR_FORM_ID'
 };
